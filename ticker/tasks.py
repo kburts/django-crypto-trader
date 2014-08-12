b2348bbe520789fcf40fb4a26599bf46b44b5eae
kburts <kevin.burton2@gmail.com>
2014-08-12 11:16:43 -0700

    Added functionality to fill data holes.

--- ticker/tasks.py
+++ ticker/tasks.py
@@ -2,7 +2,7 @@ import time
 import requests
 
 from django_cryptos.celery import app
-from .models import Exchange, Pair, Ticker
+from .models import Pair, Ticker
 
 
 @app.task
@@ -12,6 +12,7 @@ def hello_world():
 @app.task
 def pollAPI(pair):
     """
+    Get exchange data for a single Pair object.
     exchange: exchange name
     @param: pair Pair object
     """
@@ -26,7 +27,7 @@ def pollAPI(pair):
 @app.task
 def pollAllAPI():
     """
-    Polls all external API's in a list
+    Polls all external API's (every Pair)
     """
     pairs = Pair.objects.all()
     for pair in pairs:
@@ -48,12 +49,52 @@ def createCSV(pairpk):
         for item in out:
             f.write(item)
 
+@app.task
+def calcTickersToAdd(ticker1, ticker2):
+    """
+    @param: ticker1: later ticker
+    @param: ticker2: earlier ticker
+    """
+    LIVE = False
+    timediff = float(ticker1.timestamp) - float(ticker2.timestamp)
+    numToAdd = int(timediff) / 10
+    for x in xrange(1, numToAdd):
+        timestamp = float(ticker2.timestamp) + 10*x,
+        priceDiff = float(ticker1.price) - float(ticker2.price)
+        price = float(ticker2.price) + (priceDiff / x)
+        if LIVE:
+            Ticker.objects.create(
+                pair = ticker1.pair,
+                timestamp = timestamp,
+                price = price)
+        else:
+            print ticker1.pair, timestamp, price, ">", ticker1.price, "<>", ticker2.price, "<"
+
+
 
 @app.task
-def fillDataHoles():
+def fillDataHoles(pair):
+    allowedDataGap = 15
+    data = Ticker.objects.filter(pair=pair.pk)
+    for x in xrange(len(data)-2): #all but the last one.
+        t1 = float(data[x].timestamp)
+        t2 = float(data[x+1].timestamp)
+        ## Check if later timestamp is greater than sooner one by more than 15 seconds
+        if t1 > t2 + allowedDataGap:
+            calcTickersToAdd(data[x], data[x+1])
+            #timediff = int((t1 - t2)/10)
+            #print timediff
+
+
+@app.task
+def fillAllDataHoles():
     """
     Attempts to fill holes in data caused by server shutdown,
-        or unavailability to connect to the external API
+        or unavailability to connect to the external API.
+    If distance between data points is > 15 seconds, it adds more in the middle.
     Might take a long time!
     """
-    pass
+    pairs = Pair.objects.all()
+    for pair in pairs:
+        #fillDataHoles.delay(pair)
+        fillDataHoles(pair)
